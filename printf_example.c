5b7cd1d36706bff7cf4323e3dfe8d8d619d23541
Bojidar Tenev <bojidar_tenev@yahoo.com>
2025-11-17 21:09:26 +0000

    printf/scanf | input/output basic examples

---
+++ printf_example.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	// string literal 
+	printf("Hello, my name is %s\n", "Bubu");
+	// integer
+	printf("Printing some integers: %d, %d\n", 1, 2);
+	// float
+	printf("Printing some float: %g\n", 1.1);
+	// char
+	printf("Printing some char: %c\n", 'a');
+
+    return 0;
+}
---
+++ scanf_example.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	// pre-declaring the variables in which input will be stored
+	int num1;
+
+	printf("Enter an integer: \n");
+	// & used to store into num1
+	scanf("%d", &num1);
+
+	printf("Your number: %d", num1);
+   
+	 return 0;
+}
